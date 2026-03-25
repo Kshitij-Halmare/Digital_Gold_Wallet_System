1593cc4a8ba82021b7cadee2054276f2cdc04387
Pratham Ranjan Maske <121380335+pratm1304@users.noreply.github.com>
2026-03-25 16:15:13 +0530

    Pratham feature (#5)
    * Boiler Plate
    
    * Users & Virtual Holdings table added
    
    * Added jpa users and vholdings

--- src/main/java/com/example/Digital_Gold_Wallet_System/entity/VendorBranches.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/entity/VendorBranches.java
@@ -1,7 +1,6 @@
 package com.example.Digital_Gold_Wallet_System.entity;
 
 import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -15,11 +14,5 @@ public class VendorBranches {
     @Column(name = "createdAt")
     private LocalDateTime createdAt;
 
-//    @ManyToOne
-//    @JoinColumn(name = "vendor_id")
-//    private Vendors vendor;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "address_id")
-//    private Addresses addresses;
 }
+
