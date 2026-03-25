e3b364b28d90eabd3da744a89dad5e683ef42b27
Kshitij Halmare <kshitij.halmare1@gmail.com>
2026-03-25 20:22:09 +0530

    All get api of vendor branch and address completed

--- src/main/java/com/example/Digital_Gold_Wallet_System/entity/Addresses.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/entity/Addresses.java
@@ -4,26 +4,26 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.generator.values.internal.GeneratedValuesImpl;
 
-@Data
+import java.util.List;
+
 @Entity
+@Data
 public class Addresses {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "address_id")
     private Integer addressId;
 
-    @Column(name = "street", length = 255)
     private String street;
-
-    @Column(name = "city", length = 100)
     private String city;
-
-    @Column(name = "state", length = 100)
     private String state;
 
-    @Column(name = "postal_code", length = 20)
+    @Column(name = "postal_code")
     private String postalCode;
 
-    @Column(name = "country", length = 100)
     private String country;
+
+    @OneToMany(mappedBy = "addressess", cascade = CascadeType.ALL)
+    private List<VendorBranches> vendorBranches;
 }
