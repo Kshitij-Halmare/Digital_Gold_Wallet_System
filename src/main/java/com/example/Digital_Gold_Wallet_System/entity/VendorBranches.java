66ef6d99bef88d5e1f474a492f2a39ec454a9931
poharesanika <poharesanika31@gmail.com>
2026-03-25 15:55:05 +0530

    added jpa entities Vendor and VendorBranches

--- src/main/java/com/example/Digital_Gold_Wallet_System/entity/VendorBranches.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/entity/VendorBranches.java
@@ -1,5 +1,25 @@
 package com.example.Digital_Gold_Wallet_System.entity;
 
-public class VendorBranches {
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class VendorBranches {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer branchId;
+    @Column(precision = 18, scale = 2)
+    private BigDecimal quantity;
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+//    @ManyToOne
+//    @JoinColumn(name = "vendor_id")
+//    private Vendors vendor;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "address_id")
+//    private Addresses addresses;
 }
