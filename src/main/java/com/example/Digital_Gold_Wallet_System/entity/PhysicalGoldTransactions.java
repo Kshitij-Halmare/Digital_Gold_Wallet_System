84ef77f75be5a53d9ceba2490aea422dcf6bd4db
Kshitij-Halmare <horizon29042004@gmail.com>
2026-03-25 15:49:02 +0530

    Added JPA Phusical gold entitie for gold wallet system (#2)
    Co-authored-by: Kshitij Halmare <kshitij.halmare1@gmail.com>

--- src/main/java/com/example/Digital_Gold_Wallet_System/entity/PhysicalGoldTransactions.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/entity/PhysicalGoldTransactions.java
@@ -1,5 +1,17 @@
 package com.example.Digital_Gold_Wallet_System.entity;
 
-public class PhysicalGoldTransactions {
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class PhysicalGoldTransactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int transaction_id;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal quantity;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
