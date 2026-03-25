a381b0450f63fba223301c26d138d7adab2770e7
Kshitij Halmare <kshitij.halmare1@gmail.com>
2026-03-25 15:17:47 +0530

    Initial commit - new project

---
+++ src/main/java/com/example/Digital_Gold_Wallet_System/DigitalGoldWalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Digital_Gold_Wallet_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGoldWalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGoldWalletSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Digital_Gold_Wallet_System/DigitalGoldWalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Digital_Gold_Wallet_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalGoldWalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
