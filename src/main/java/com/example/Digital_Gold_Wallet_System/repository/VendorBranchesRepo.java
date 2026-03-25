52d92a1ba6224285563d648e9c530d4325d324f9
Kshitij-Halmare <horizon29042004@gmail.com>
2026-03-25 16:36:47 +0530

    Feature/kshitij halmare (#10)
    * Added JPA Phusical gold entitie for gold wallet system
    
    * Done Completed repo
    
    * Done Completed repo
    
    ---------
    
    Co-authored-by: Kshitij Halmare <kshitij.halmare1@gmail.com>

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorBranchesRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorBranchesRepo.java
@@ -1,5 +1,8 @@
 package com.example.Digital_Gold_Wallet_System.repository;
 
-public class VendorBranchesRepo {
+import com.example.Digital_Gold_Wallet_System.entity.VendorBranches;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorBranchesRepo extends JpaRepository<VendorBranches, Integer> {
 
 }
--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorsRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorsRepo.java
@@ -1,5 +1,9 @@
 package com.example.Digital_Gold_Wallet_System.repository;
 
-public class VendorsRepo {
+import com.example.Digital_Gold_Wallet_System.entity.VendorBranches;
+import com.example.Digital_Gold_Wallet_System.entity.Vendors;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  VendorsRepo extends JpaRepository<Vendors, Integer> {
 
 }
