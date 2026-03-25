91d58078186654c9f52dd90c439476e24f7b5e51
pratm1304 <prathammaske1304@gmail.com>
2026-03-25 22:20:41 +0530

    added user add test

--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/UsersControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/UsersControllerTest.java
@@ -4,16 +4,16 @@ import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+
 @Transactional
 public class UsersControllerTest {
 
