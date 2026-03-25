d79da3973361626e105eff77fd80c0abce739e91
Prathmesh Hande <146568335+void173@users.noreply.github.com>
2026-03-25 16:46:15 +0530

    added payment and address entity (#11)

---
+++ src/main/java/com/example/Digital_Gold_Wallet_System/enums/PaymentMethod.java
@@ -0,0 +1,22 @@
+package com.example.Digital_Gold_Wallet_System.enums;
+
+public enum PaymentMethod {
+
+    CREDIT_CARD("Credit Card"),
+    DEBIT_CARD("Debit Card"),
+    GOOGLE_PAY("Google Pay"),
+    AMAZON_PAY("Amazon Pay"),
+    PHONEPE("PhonePe"),
+    PAYTM("Paytm"),
+    BANK_TRANSFER("Bank Transfer");
+
+    private final String value;
+
+    PaymentMethod(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
