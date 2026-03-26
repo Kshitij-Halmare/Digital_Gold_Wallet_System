38645949869fdf27c998b74c13103b9b4167f0cf
Pratham Ranjan Maske <121380335+pratm1304@users.noreply.github.com>
2026-03-26 20:59:17 +0530

    Pratham feature (#54)
    * Changed Users & VirtualHoldings
    
    * Modified users & vHoldings
    
    * added modelMapper and used local db
    
    * test userAddPositive testcase
    
    * added user add test
    
    * add new payment positive testcase working
    
    * add new address positive testcase working
    
    * Updated POM.xml
    
    * testing vendor apis
    
    * testing vendor apis
    
    * added 4 entity with mappings
    
    * added 2 methods in userTestController & all test working
    
    ---------
    
    Co-authored-by: Kshitij-Halmare <horizon29042004@gmail.com>

--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/UsersControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/UsersControllerTest.java
@@ -1,11 +1,17 @@
 package com.example.Digital_Gold_Wallet_System.controller;
 
+import com.example.Digital_Gold_Wallet_System.entity.Users;
+import com.example.Digital_Gold_Wallet_System.repository.UsersRepo;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -13,78 +19,87 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Transactional
 class UsersControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    void testGetAllUsers() throws Exception {
-//
-//        mockMvc.perform(get("/users"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void testCreateUser() throws Exception {
-//
-//        String userJson = """
-//        {
-//            "email": "test@gmail.com",
-//            "name": "Pratham"
-//        }
-//        """;
-//
-//        mockMvc.perform(post("/users")
-//                        .contentType("application/json")
-//                        .content(userJson))
-//                .andExpect(status().isCreated());
-//    }
-//
-//    @Test
-//    void testGetUserById() throws Exception {
-//
-//        mockMvc.perform(get("/users/2"))
-//                .andExpect(status().isOk());
-//
-//    }
-//
-//    @Test
-//    void testUpdateUser() throws Exception {
-//
-//        String userJson = """
-//        {
-//            "email": "updated@gmail.com",
-//            "name": "UpdatedUser",
-//            "balance": 2000.00
-//        }
-//        """;
-//
-//        mockMvc.perform(put("/users/2")
-//                        .contentType("application/json")
-//                        .content(userJson))
-//                .andExpect(status().isNoContent());
-//    }
-//
-//    @Test
-//    void testUpdateUserBalance() throws Exception {
-//
-//        String userJson = """
-//        {
-//            "balance": 2000.00
-//        }
-//        """;
-//
-//        mockMvc.perform(put("/users/2")
-//                        .contentType("application/json")
-//                        .content(userJson))
-//                .andExpect(status().isNoContent());
-//    }
-//
-//    @Test
-//    void testDeleteUser() throws Exception {
-//
-//        mockMvc.perform(delete("/users/2"))
-//                .andExpect(status().isNoContent());
-//    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UsersRepo usersRepository;
+
+    private Users createTestUser() {
+        Users user = new Users();
+        user.setName("TestUser");
+        user.setEmail("test@gmail.com");
+        user.setBalance(BigDecimal.valueOf(1000));
+        return usersRepository.save(user);
+    }
+
+    @Test
+    @DisplayName("Test Get All Users")
+    void testGetAllUsers() throws Exception {
+
+        createTestUser();
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test Create User")
+    void testCreateUser() throws Exception {
+
+        String userJson = """
+        {
+            "name": "Pratham",
+            "email": "pratham@gmail.com",
+            "balance": 5000
+        }
+        """;
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("Test Find User By ID")
+    void testGetUserById() throws Exception {
+
+        Users savedUser = createTestUser();
+
+        mockMvc.perform(get("/users/" + savedUser.getUserId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test Update User")
+    void testUpdateUser() throws Exception {
+
+        Users savedUser = createTestUser();
+
+        String updatedJson = """
+        {
+            "userName": "UpdatedUser",
+            "userEmail": "updated@gmail.com",
+            "userBalance": 2000
+        }
+        """;
+
+        mockMvc.perform(put("/users/" + savedUser.getUserId())
+                        .contentType("application/json")
+                        .content(updatedJson))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("Test Delete User")
+    void testDeleteUser() throws Exception {
+
+        Users savedUser = createTestUser();
+
+        mockMvc.perform(delete("/users/" + savedUser.getUserId()))
+                .andExpect(status().isNoContent());
+    }
 
 }
