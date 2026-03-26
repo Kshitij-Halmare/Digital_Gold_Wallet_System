e8e745327b7c22b30916633f5807a68c59a07623
poharesanika <poharesanika31@gmail.com>
2026-03-26 11:04:27 +0530

    Add test case

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorsRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorsRepo.java
@@ -4,6 +4,8 @@ import com.example.Digital_Gold_Wallet_System.entity.VendorBranches;
 import com.example.Digital_Gold_Wallet_System.entity.Vendors;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface  VendorsRepo extends JpaRepository<Vendors, Integer> {
+import java.util.List;
 
+public interface  VendorsRepo extends JpaRepository<Vendors, Integer> {
+    List<Vendors> findByVendorName(String vendorName);
 }
