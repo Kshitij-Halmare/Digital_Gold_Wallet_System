a55eea6c288ba7064e9d437266fd780184cab4a8
pratm1304 <prathammaske1304@gmail.com>
2026-03-26 15:06:01 +0530

    testing vendor apis

--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/VendorsControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/VendorsControllerTest.java
@@ -1,5 +1,36 @@
 package com.example.Digital_Gold_Wallet_System.controller;
 
+import jakarta.persistence.Column;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+@AutoConfigureMockMvc
+@Transactional
+@SpringBootTest
 public class VendorsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testCreateVendor() throws Exception{
+        String addVendorJson = """
+                    "vendorName":"Tanishq CO.",
+                    "description":"Best in NGP",
+                    "contactPersonName":"Aman",
+                    "contactEmail":"aman@gmail.com",
+                    "contactPhone":"6577362454",
+                    "websiteUrl":,
+                    "totalGoldQuantity":,
+                    "currentGoldPrice":
+                """
+    }
 
 }
