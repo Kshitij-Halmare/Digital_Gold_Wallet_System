7436163ebc2c9c1df9eb558e9af156131ee8e85c
pratm1304 <prathammaske1304@gmail.com>
2026-03-26 14:22:36 +0530

    Updated POM.xml

--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/UsersControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/UsersControllerTest.java
@@ -1,5 +1,50 @@
 package com.example.Digital_Gold_Wallet_System.controller;
 
-public class UsersControllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UsersControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetAllUsers() throws Exception {
+
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    void testCreateUser() throws Exception {
+
+        String userJson = """
+        {
+            "email": "test@gmail.com",
+            "name": "Pratham"
+        }
+        """;
+
+        mockMvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void testGetUserById() throws Exception {
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk());
+    }
+
 
 }
