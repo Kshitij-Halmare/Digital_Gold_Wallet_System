4adb5eb2e659eaa72d763c2fe5e5a39fa7bdcca6
sanika-jain <134360422+sanika-jain@users.noreply.github.com>
2026-03-26 17:23:16 +0530

    Sanika feature (#47)
    * entity
    
    * added testcases
    
    * Merge main into sanika-feature
    
    * Code fixed
    
    * yaml fixed

--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/VirtualGoldHoldingsControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/VirtualGoldHoldingsControllerTest.java
@@ -1,52 +1,11 @@
 package com.example.Digital_Gold_Wallet_System.controller;
 
-<<<<<<< HEAD
-import com.example.Digital_Gold_Wallet_System.entity.VirtualGoldHoldings;
-import com.example.Digital_Gold_Wallet_System.repository.VirtualGoldHoldingsRepo;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-public class VirtualGoldHoldingsControllerTest{
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private VirtualGoldHoldingsRepo repository;
-
-    @Test
-    void testGetAllHoldings_Positive() throws Exception{
-        VirtualGoldHoldings h = new VirtualGoldHoldings();
-        h.setQuantity(BigDecimal.valueOf(10));
-        h.setCreatedAt(LocalDateTime.now());
-        repository.save(h);
-
-        mockMvc.perform(get("/virtual_gold_holdings"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$._embedded.virtualGoldHoldingses").exists());
-    }
-
-    @Test
-    void testGetAllHoldings_Empty() throws Exception{
-        mockMvc.perform(get("virtual_gold_holdings"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$._embedded.virtual__gold_holdings").doesNotExist());
-    }
-=======
 public class VirtualGoldHoldingsControllerTest{
 
->>>>>>> 9797fb572b0d9603b07b6202366c7d7b9801a95c
 }
