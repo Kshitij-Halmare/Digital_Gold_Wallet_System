5710cd6bdbac94ffacabe1927462fd93aad48867
Prathmesh_H <handeprathmesh1@gmail.com>
2026-03-26 10:56:35 +0530

    added virtual gold holdings repo test

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/PaymentsRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/PaymentsRepo.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface PaymentsRepo extends JpaRepository<Payments, Integer> {
     List<Payments> findByPaymentStatus(PaymentStatus paymentStatus);
 
