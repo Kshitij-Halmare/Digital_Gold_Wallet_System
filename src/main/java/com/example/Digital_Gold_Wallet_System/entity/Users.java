c879c67c4c0fb88394729802d0e0731a44326561
Pratham Ranjan Maske <121380335+pratm1304@users.noreply.github.com>
2026-03-26 20:59:17 +0530

    Pratham feature (#54)
    * Changed Users & VirtualHoldings
    
    * Modified users & vHoldings
    
    * added modelMapper and used local db
    
    * test userAddPositive testcase
    
    * added user add test
    
    * add new payment positive testcase working
    
    * add new address positive testcase working
    
    * Updated POM.xml
    
    * testing vendor apis
    
    * testing vendor apis
    
    * added 4 entity with mappings
    
    * added 2 methods in userTestController & all test working
    
    ---------
    
    Co-authored-by: Kshitij-Halmare <horizon29042004@gmail.com>
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/com/example/Digital_Gold_Wallet_System/entity/Users.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/entity/Users.java
@@ -5,6 +5,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Entity
@@ -14,10 +18,17 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be empty")
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @NotBlank(message = "Name cannot be empty")
     private String name;
 
     @Column(precision = 18, scale = 2)
+    @PositiveOrZero(message = "Balance should be greater than or equal to 0")
     private BigDecimal balance;
     @Column(columnDefinition = "DATETIME", name="createdAt")
     private LocalDateTime createdAt;
