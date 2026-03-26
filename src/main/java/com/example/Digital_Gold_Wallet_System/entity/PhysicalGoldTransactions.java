f62bc55b5310c6369cabfb552a61545e7783d94c
sanika-jain <134360422+sanika-jain@users.noreply.github.com>
2026-03-26 11:41:33 +0530

    added testcases

--- src/main/java/com/example/Digital_Gold_Wallet_System/entity/PhysicalGoldTransactions.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/entity/PhysicalGoldTransactions.java
@@ -1,10 +1,13 @@
 package com.example.Digital_Gold_Wallet_System.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class PhysicalGoldTransactions {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/example/Digital_Gold_Wallet_System/repository/PhysicalGoldTransactionsRepoTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/repository/PhysicalGoldTransactionsRepoTest.java
@@ -1,5 +1,103 @@
 package com.example.Digital_Gold_Wallet_System.repository;
 
+import com.example.Digital_Gold_Wallet_System.entity.PhysicalGoldTransactions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PhysicalGoldTransactionsRepoTest {
 
+
+    @Autowired
+    PhysicalGoldTransactionsRepo physicalGoldTransactionsRepo;
+
+
+    @AfterEach
+    void cleanup(){
+        physicalGoldTransactionsRepo.deleteAll();
+    }
+
+
+    @Test
+    void testSave(){
+        //positive scenario for saving
+
+
+        PhysicalGoldTransactions physicalGoldTransactions=new PhysicalGoldTransactions();
+        physicalGoldTransactions.setQuantity(BigDecimal.valueOf(300));
+        physicalGoldTransactions.setCreatedAt(LocalDateTime.now());
+
+
+        PhysicalGoldTransactions saved=physicalGoldTransactionsRepo.save(physicalGoldTransactions);
+
+
+        assertThat(saved.getTransactionId()).isNotNull();
+        assertThat(saved.getQuantity()).isEqualByComparingTo(physicalGoldTransactions.getQuantity());
+
+
+    }
+
+
+    @Test
+    void testFindById(){
+        //positive scenario for find by id
+
+
+        PhysicalGoldTransactions physicalGoldTransactions=new PhysicalGoldTransactions();
+        physicalGoldTransactions.setQuantity(BigDecimal.valueOf(200));
+        physicalGoldTransactions.setCreatedAt(LocalDateTime.now());
+
+
+        PhysicalGoldTransactions saved=physicalGoldTransactionsRepo.save(physicalGoldTransactions);
+
+
+        Optional<PhysicalGoldTransactions> fetched=physicalGoldTransactionsRepo.findById(saved.getTransactionId());
+
+
+        assertThat(fetched).isPresent();
+        assertThat(fetched.get().getQuantity()).isEqualByComparingTo(physicalGoldTransactions.getQuantity());
+
+
+
+
+    }
+
+
+    @Test
+    void testInvalidIdForFindById(){
+        //negative scenario for find by id
+
+
+        PhysicalGoldTransactions physicalGoldTransactions=new PhysicalGoldTransactions();
+        physicalGoldTransactions.setQuantity(BigDecimal.valueOf(200));
+        physicalGoldTransactions.setCreatedAt(LocalDateTime.now());
+
+
+        PhysicalGoldTransactions saved=physicalGoldTransactionsRepo.save(physicalGoldTransactions);
+
+
+        //creation of invalid id
+        int invalidId=saved.getTransactionId()+1000;
+
+
+        Optional<PhysicalGoldTransactions> fetched=physicalGoldTransactionsRepo.findById(invalidId);
+
+
+        assertThat(fetched).isEmpty();
+
+
+    }
+
+
 }
+
