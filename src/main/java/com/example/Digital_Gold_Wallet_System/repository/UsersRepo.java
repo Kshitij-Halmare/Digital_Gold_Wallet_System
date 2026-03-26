8f718a31552a669727a0c8cc377b1afefef3f205
Pratham Ranjan Maske <121380335+pratm1304@users.noreply.github.com>
2026-03-26 14:27:29 +0530

    updared pom (#36)
    * Changed Users & VirtualHoldings
    
    * Modified users & vHoldings
    
    * added modelMapper and used local db
    
    * test userAddPositive testcase
    
    * added user add test
    
    * add new payment positive testcase working
    
    * add new address positive testcase working
    
    * Updated POM.xml

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/UsersRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/UsersRepo.java
@@ -2,7 +2,9 @@ package com.example.Digital_Gold_Wallet_System.repository;
 
 import com.example.Digital_Gold_Wallet_System.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "users")
 public interface UsersRepo extends JpaRepository<Users, Integer> {
 
 }
