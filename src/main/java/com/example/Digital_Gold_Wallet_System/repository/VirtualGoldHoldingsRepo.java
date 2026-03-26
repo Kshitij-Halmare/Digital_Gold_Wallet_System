b23ca26c2c39414cfe0327ca90619fc2712f9496
Prathmesh_H <handeprathmesh1@gmail.com>
2026-03-26 15:09:05 +0530

    added api tests

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/VirtualGoldHoldingsRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/VirtualGoldHoldingsRepo.java
@@ -2,7 +2,9 @@ package com.example.Digital_Gold_Wallet_System.repository;
 
 import com.example.Digital_Gold_Wallet_System.entity.VirtualGoldHoldings;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "virtual_gold_holdings")
 public interface VirtualGoldHoldingsRepo extends JpaRepository<VirtualGoldHoldings, Integer> {
 
 }
--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/VirtualGoldHoldingsControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/VirtualGoldHoldingsControllerTest.java
@@ -1,5 +1,40 @@
 package com.example.Digital_Gold_Wallet_System.controller;
 
-public class VirtualGoldHoldingsControllerTest{
+import com.example.Digital_Gold_Wallet_System.entity.VirtualGoldHoldings;
+import com.example.Digital_Gold_Wallet_System.repository.VirtualGoldHoldingsRepo;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class VirtualGoldHoldingsControllerTest{
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private VirtualGoldHoldingsRepo repository;
+
+    @Test
+    void testGetAllHoldings_Positive() throws Exception{
+        VirtualGoldHoldings h = new VirtualGoldHoldings();
+        h.setQuantity(BigDecimal.valueOf(10));
+        h.setCreatedAt(LocalDateTime.now());
+        repository.save(h);
+
+        mockMvc.perform(get("/virtual_gold_holdings"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.virtualGoldHoldingses").exists());
+    }
 }
