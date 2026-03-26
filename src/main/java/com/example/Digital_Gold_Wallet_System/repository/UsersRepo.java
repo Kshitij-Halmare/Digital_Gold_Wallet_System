764b2986e604f18308f749e285a397b43fa1ff35
pratm1304 <prathammaske1304@gmail.com>
2026-03-26 21:52:28 +0530

    added prePersist and CORS

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/UsersRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/UsersRepo.java
@@ -3,10 +3,12 @@ package com.example.Digital_Gold_Wallet_System.repository;
 import com.example.Digital_Gold_Wallet_System.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RepositoryRestResource(path = "users")
+@CrossOrigin(origins = "*")
 public interface UsersRepo extends JpaRepository<Users, Integer> {
 
     List<Users> findByNameContainingIgnoreCase(String name);
