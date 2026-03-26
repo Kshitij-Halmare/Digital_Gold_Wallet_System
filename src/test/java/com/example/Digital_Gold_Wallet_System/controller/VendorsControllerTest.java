f13a8a73d02b56cc96c12e4df4e15d534dc1f29d
Kshitij Halmare <kshitij.halmare1@gmail.com>
2026-03-26 15:48:49 +0530

    yaml done

--- src/test/java/com/example/Digital_Gold_Wallet_System/controller/VendorsControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/controller/VendorsControllerTest.java
@@ -1,5 +1,22 @@
 package com.example.Digital_Gold_Wallet_System.controller;
 
+
+import com.example.Digital_Gold_Wallet_System.entity.Vendors;
+import com.example.Digital_Gold_Wallet_System.repository.VendorsRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class VendorsControllerTest {
 
 }
