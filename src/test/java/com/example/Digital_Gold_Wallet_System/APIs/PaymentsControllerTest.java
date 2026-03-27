fbe037de5875bd1731f789a74da88316b853d116
Prathmesh_H <handeprathmesh1@gmail.com>
2026-03-27 15:49:52 +0530

    changes

--- src/test/java/com/example/Digital_Gold_Wallet_System/APIs/PaymentsControllerTest.java
+++ src/test/java/com/example/Digital_Gold_Wallet_System/APIs/PaymentsControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -97,6 +98,88 @@ public class PaymentsControllerTest {
                 .andExpect(status().isOk());
     }
 
-//    @Test
-//    void
+    @Test
+    void testGetSuccessfullPayments_Positive() throws Exception{
+        Users u = createUser();
+        createPayment(u, BigDecimal.valueOf(100), PaymentStatus.SUCCESS, PaymentMethod.UPI);
+
+        mockMvc.perform(get("/payments/search/findByPaymentStatus")
+                .param("paymentStatus","SUCCESS"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.payments").exists());
+    }
+
+    @Test
+    void testGetSuccessfullPayments_Negative() throws Exception{
+        mockMvc.perform(get("/payments/search/findByPaymentStatus")
+                .param("paymentStatus","SUCCESS"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.payments").isEmpty());
+    }
+
+    @Test
+    void testGetFailedPayments_Positive() throws Exception{
+        Users user = createUser();
+        createPayment(user,BigDecimal.valueOf(100),PaymentStatus.FAILED,PaymentMethod.UPI);
+
+        mockMvc.perform(get("/payments/search/findByPaymentStatus")
+                .param("paymentStatus","FAILED"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.payments").exists());
+    }
+
+    @Test
+    void testGetFailedPayments_Negative() throws Exception{
+
+        mockMvc.perform(get("/payments/search/findByPaymentStatus")
+                        .param("paymentStatus","FAILED"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.payments").isEmpty());
+    }
+
+    @Test
+    void testGetPaymentsByMethod_Positive() throws Exception{
+        Users user = createUser();
+        createPayment(user,BigDecimal.valueOf(100),PaymentStatus.FAILED,PaymentMethod.UPI);
+
+        mockMvc.perform(get("/payments/search/findByPaymentMethod")
+                        .param("paymentMethod","UPI"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._embedded.payments").exists());
+    }
+
+    @Test
+    void testGetPaymentsByMethod_Negative() throws Exception{
+
+        mockMvc.perform(get("/payments/search/findByPaymentMethod")
+                        .param("paymentMethod","XYZ"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testAddPayment_Positive() throws Exception {
+
+        Users user = createUser();
+
+        String json = """
+{
+  "amount": 100,
+  "paymentMethod": "UPI",
+  "paymentStatus": "SUCCESS",
+  "paymentTransactionType": "CREDIT",
+  "createdAt": "2026-03-27T10:00:00",
+  "_links": {
+    "user": {
+      "href": "http://localhost/users/%d"
+    }
+  }
+}
+""".formatted(user.getUserId());
+
+        mockMvc.perform(post("/payments")
+                        .contentType("application/json")
+                        .accept("application/hal+json")
+                        .content(json))
+                .andExpect(status().isCreated());
+    }
 }
