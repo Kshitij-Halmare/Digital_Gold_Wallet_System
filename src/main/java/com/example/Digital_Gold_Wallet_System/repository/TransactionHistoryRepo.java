6c3545120291e3a710cb50d8b2343b8e01d9be9b
Prathmesh_H <handeprathmesh1@gmail.com>
2026-03-30 17:05:45 +0530

    changes

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/TransactionHistoryRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/TransactionHistoryRepo.java
@@ -4,13 +4,14 @@ import com.example.Digital_Gold_Wallet_System.entity.TransactionHistory;
 import com.example.Digital_Gold_Wallet_System.entity.enums.TransactionType;
 import com.example.Digital_Gold_Wallet_System.entity.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(path = "transactions")
 @CrossOrigin(origins = "*")
 public interface TransactionHistoryRepo extends JpaRepository<TransactionHistory, Integer> {
     List<TransactionHistory> findByBranchBranchId(Integer branchId);
