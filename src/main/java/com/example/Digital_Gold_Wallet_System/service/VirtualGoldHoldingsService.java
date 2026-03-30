209f44131aa56fae9d6d32ff7e0409532e6ff46c
pratm1304 <prathammaske1304@gmail.com>
2026-03-30 13:09:34 +0530

    working on service

--- src/main/java/com/example/Digital_Gold_Wallet_System/service/VirtualGoldHoldingsService.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/service/VirtualGoldHoldingsService.java
@@ -1,17 +1,28 @@
 package com.example.Digital_Gold_Wallet_System.service;
 
-import com.example.Digital_Gold_Wallet_System.entity.PhysicalGoldTransactions;
-import com.example.Digital_Gold_Wallet_System.entity.VirtualGoldHoldings;
+import com.example.Digital_Gold_Wallet_System.entity.*;
+import com.example.Digital_Gold_Wallet_System.exception.InsufficientGoldException;
 import com.example.Digital_Gold_Wallet_System.repository.PhysicalGoldTransactionsRepo;
+import com.example.Digital_Gold_Wallet_System.repository.UsersRepo;
+import com.example.Digital_Gold_Wallet_System.repository.VendorBranchesRepo;
 import com.example.Digital_Gold_Wallet_System.repository.VirtualGoldHoldingsRepo;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Service
 public class VirtualGoldHoldingsService {
+
+    @Autowired
+    private UsersRepo usersRepo;
+
+    @Autowired
+    private VendorBranchesRepo vendorBranchesRepo;
+
     @Autowired
     private VirtualGoldHoldingsRepo holdingRepo;
 
@@ -21,21 +32,16 @@ public class VirtualGoldHoldingsService {
     @Transactional
     public void convertToPhysical(Integer holdingId) {
 
-        // ✅ 1. Fetch holding
-        VirtualGoldHoldings holding = holdingRepo.findById(holdingId)
-                .orElseThrow(() -> new RuntimeException("Holding not found"));
+        VirtualGoldHoldings holding = holdingRepo.findById(holdingId).orElseThrow(() -> new RuntimeException("Holding not found"));
 
-        // ❌ 2. Already converted
         if (holding.isConverted()) {
             throw new RuntimeException("Already converted");
         }
 
-        // ❌ 3. Delivery address validation
         if (holding.getUser() == null || holding.getUser().getAddress() == null) {
             throw new RuntimeException("Delivery address required");
         }
 
-        // ✅ 4. Create physical transaction
         PhysicalGoldTransactions txn = new PhysicalGoldTransactions();
         txn.setUser(holding.getUser());
         txn.setBranch(holding.getBranch());
@@ -45,8 +51,33 @@ public class VirtualGoldHoldingsService {
 
         txnRepo.save(txn);
 
-        // ✅ 5. Update holding
+//        VendorBranches vendorBranch = holdinf;
+
         holding.setConverted(true);
         holdingRepo.save(holding);
+    }
+
+    @Transactional
+    public void buyVirtualGoldHoldings(Integer userId, BigDecimal qty, Integer branchId) {
+
+        Users user = usersRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        VendorBranches branch = vendorBranchesRepo.findById(branchId).orElseThrow(() -> new ResourceNotFoundException("Branch not found"));
+
+        Vendors vendor = branch.getVendors();
+
+        if(qty.compareTo(branch.getQuantity()) == 1) {
+            throw new InsufficientGoldException("Not enough gold in the given branch...");
+        }
+
+        VirtualGoldHoldings holding = new VirtualGoldHoldings();
+        holding.setQuantity(qty);
+        holding.setCreatedAt(LocalDateTime.now());
+        holding.setUser(user);
+        holding.setBranch(branch);
+        holding.setConverted(false);
+
+        holdingRepo.save(holding);
+
     }
 }
