3fa6120eff5b49593e4c6b10967f2cf9856be8f0
sanika-jain <134360422+sanika-jain@users.noreply.github.com>
2026-03-30 16:29:19 +0530

    added update quantity

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorsRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/VendorsRepo.java
@@ -16,7 +16,6 @@ import java.util.List;
 )
 public interface VendorsRepo extends JpaRepository<Vendors, Integer> {
     List<Vendors> findByVendorName(String vendorName);
-    //List<VendorsProjection> findAllProjectedBy();
 
     //Search bar
     @RestResource(path = "findByVendorNameContainingIgnoreCase")
