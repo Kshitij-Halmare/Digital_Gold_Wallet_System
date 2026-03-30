1afd50e014eb9610cbc08a78b51c8989c0bc71be
pratm1304 <prathammaske1304@gmail.com>
2026-03-30 10:50:43 +0530

    added CORS ORIGIN in every Repo file

--- src/main/java/com/example/Digital_Gold_Wallet_System/repository/AddressesRepo.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/repository/AddressesRepo.java
@@ -4,8 +4,10 @@ import com.example.Digital_Gold_Wallet_System.entity.Addresses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(path = "addresses")
+@CrossOrigin(origins = "*")
 public interface AddressesRepo extends JpaRepository<Addresses, Integer> {
 
 }
