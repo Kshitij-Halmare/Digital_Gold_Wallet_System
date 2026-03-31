621cbe37994052dcd56633d99f747b0887d6210f
pratm1304 <prathammaske1304@gmail.com>
2026-03-31 12:27:26 +0530

    added projections & resolved conflicts

--- src/main/java/com/example/Digital_Gold_Wallet_System/Projection/UsersProjection.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/Projection/UsersProjection.java
@@ -1,8 +1,11 @@
 package com.example.Digital_Gold_Wallet_System.Projection;
 
+import org.springframework.data.rest.core.config.Projection;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Projection(name = "userProjection", types = {UsersProjection.class})
 public interface UsersProjection {
     Integer getUserId();
     String getName();
