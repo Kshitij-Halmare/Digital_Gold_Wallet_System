e9afb13d830390d44e49de70caa87a3abfe1a547
Kshitij Halmare <kshitij.halmare1@gmail.com>
2026-04-01 14:34:30 +0530

    Done cahnges in vendorbrnach projecction

--- src/main/java/com/example/Digital_Gold_Wallet_System/Projection/VendorBranchProjection.java
+++ src/main/java/com/example/Digital_Gold_Wallet_System/Projection/VendorBranchProjection.java
@@ -2,6 +2,7 @@ package com.example.Digital_Gold_Wallet_System.Projection;
 
 import com.example.Digital_Gold_Wallet_System.entity.Addresses;
 import com.example.Digital_Gold_Wallet_System.entity.VendorBranches;
+import com.example.Digital_Gold_Wallet_System.entity.Vendors;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.math.BigDecimal;
@@ -9,7 +10,25 @@ import java.time.LocalDateTime;
 
 @Projection(name = "vendorBranchView", types = VendorBranches.class)
 public interface VendorBranchProjection {
+
+    int getBranchId();
     BigDecimal getQuantity();
     LocalDateTime getCreatedAt();
-    Addresses getAddress();
+
+    AddressView getAddress();
+    VendorView getVendors();
+
+    interface AddressView {
+        Integer getAddressId();
+        String getStreet();
+        String getCity();
+        String getState();
+        String getPostalCode();
+        String getCountry();
+    }
+
+    interface VendorView {
+        Integer getVendorId();
+        String getVendorName();
+    }
 }
